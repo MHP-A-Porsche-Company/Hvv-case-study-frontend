f8b6ffd91d389d7c938f233fdffc8060843734c1
edindelan <Limunada2011>
2024-09-13 16:34:14 +0200

    build setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,19 @@
-/** @type {import('tailwindcss').Config} */
-export default {
+const { fontFamily } = require('tailwindcss/defaultTheme')
+
+module.exports = {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Roboto', ...fontFamily.sans],
+        heading: ['Orbitron', ...fontFamily.sans],
+        mono: ['Space Mono', ...fontFamily.mono],
+      },
+      colors: {},
+    },
   },
   plugins: [],
 }
-
